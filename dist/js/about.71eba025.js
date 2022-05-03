b0cb28f7f262df480f69e8aaf87b4fc017ff56d6
Liu ZIxu <398488080@qq.com>
2022-05-03 18:37:03 +0800

    up

---
+++ dist/js/about.71eba025.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmyyy"]=self["webpackChunkmyyy"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var i=t(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.71eba025.js.map
