a5e4d78e0250f93c6a2ad22e1b5c7d64ddfa9efd
Liu Zixu <398488080@qq.com>
2022-06-22 14:55:13 +0800

    threejs

---
+++ .history/src/views/PreView_20220622142810.vue
@@ -0,0 +1,70 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable space-before-function-paren */
+<template>
+    <div id="pre">
+        <div class="liuzixu">design by LiuZixu</div>
+        <div class="cont">
+            <img src="../assets/logo.png">
+            <p class="zi">Hello，我是刘子煦</p>
+            <router-link to="/home/who">
+              <button id="start">Start</button>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+/* const geometry = new THREE.BoxGeometry( 3, 3, 3 );
+const material = new THREE.MeshBasicMaterial( { color: "rgb(20,20,20)" } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube ); */
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.update()
+
+const loader = new GLTFLoader(); 
+loader.load('blackhole/scene.gltf',function( gltf ){
+  scene.add( gltf.scene )
+},undefined,function( error ){ console.error( error );});
+
+camera.position.z = 2;
+camera.position.y = 2;
+
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  renderer.render( scene, camera );
+};
+
+animate();
+export default {
+  name: 'PreView',
+  components: {},
+  methods: {
+    homee: () => { window.location.assign('./#/home') }
+  },
+  mounted(){
+  document.getElementById("pre").appendChild(renderer.domElement);
+  const root = document.getElementsByTagName("canvas")
+  root[0].attributes.style.nodeValue = "display: block; width: 1656px; height: 600px; position:absolute; top: 600px"
+  console.log(document.getElementsByTagName("canvas"))
+  console.log(renderer.domElement)
+
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ .history/src/views/PreView_20220622144238.vue
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable space-before-function-paren */
+<template>
+    <div id="pre">
+        <div class="liuzixu">design by LiuZixu</div>
+        <div class="cont">
+            <img src="../assets/logo.png">
+            <p class="zi">Hello，我是刘子煦</p>
+            <router-link to="/home/who">
+              <button id="start">Start</button>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+/* const geometry = new THREE.BoxGeometry( 3, 3, 3 );
+const material = new THREE.MeshBasicMaterial( { color: "rgb(20,20,20)" } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube ); */
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.update()
+
+const loader = new GLTFLoader(); 
+loader.load('blackhole/scene.gltf',function( gltf ){
+  scene.add( gltf.scene )
+},undefined,function( error ){ console.error( error );});
+
+camera.position.z = 2;
+camera.position.y = -1;
+
+
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+  camera.rotation.x += 0.001
+  camera.position.z -= 0.001
+};
+
+animate();
+export default {
+  name: 'PreView',
+  components: {},
+  methods: {
+    homee: () => { window.location.assign('./#/home') }
+  },
+  mounted(){
+  document.getElementById("pre").appendChild(renderer.domElement);
+  const root = document.getElementsByTagName("canvas")
+  root[0].attributes.style.nodeValue = "display: block; width: auto; height: 800px; position:absolute; top: 0px"
+  console.log(document.getElementsByTagName("canvas"))
+  console.log(renderer.domElement)
+
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
