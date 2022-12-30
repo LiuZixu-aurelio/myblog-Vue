6abd4c727d7ca1a5bb14961ef1060ed71b1e8929
liuzixu <liuzixu@sansi.com>
2022-12-30 16:58:17 +0800

    up

---
+++ .history/src/pages/talk-shadow_20221230090335.js
@@ -0,0 +1,23 @@
+const talkShadow1 = '阴影'
+  
+
+
+const aa = require("/src/assets/talk/shadow/talk-shadow0.png")
+const ab = 'url(' + aa +')'
+const talkShadow2 = {backgroundImage: ab}
+
+
+const talkShadow4 = '/home/talk/shadow'
+
+const talkShadow3 = 
+[
+    require('/src/assets/talk/shadow/talk-shadow.png'),
+    require('/src/assets/talk/shadow/talk-shadow1.png'),
+    require('/src/assets/talk/shadow/talk-shadow2.png'),
+    require('/src/assets/talk/shadow/talk-shadow3.png'),
+    ]
+
+export { talkShadow1 }
+export { talkShadow2 }
+export { talkShadow3 }
+export { talkShadow4 }
