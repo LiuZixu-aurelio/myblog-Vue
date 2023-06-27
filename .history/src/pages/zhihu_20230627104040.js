4a1b04b53084dcb2e36a7cc701292482e2aaf7cd
liuzixu <liuzixu@sansi.com>
2023-06-27 10:41:57 +0800

    go

---
+++ .history/src/pages/zhihu_20230627104040.js
@@ -0,0 +1,47 @@
+const scard1 = '知乎直播升级 - 2021'
+  
+/* const scard2 = '{background:url(require("/src/assets/zhihu.png")}' */
+/* const scard2 = 'url( require("./src/assets/zhihu.png"))' */
+const aa = require("/src/assets/project/zhihu/zhihu.png")
+const ab = 'url(' + aa +')'
+const scard2 = {backgroundImage: ab}
+
+
+const scard3 = [
+    require('/src/assets/project/zhihu/zhihu.png'),
+    require('/src/assets/project/zhihu/zhihu2.png'),
+    require('/src/assets/project/zhihu/zhihu3.png'),
+    require('/src/assets/project/zhihu/zhihu4.png'),
+    require('/src/assets/project/zhihu/zhihu5.png'),
+    require('/src/assets/project/zhihu/zhihu6.png'),
+    require('/src/assets/project/zhihu/zhihu7.png'),
+    require('/src/assets/project/zhihu/zhihu8.png'),
+    require('/src/assets/project/zhihu/zhihu9.png'),
+    require('/src/assets/project/zhihu/zhihu10.png'),
+    require('/src/assets/project/zhihu/zhihu11.png'),
+    require('/src/assets/project/zhihu/zhihu12.png'),
+    require('/src/assets/project/zhihu/zhihu13.png'),
+    require('/src/assets/project/zhihu/zhihu14.png'),
+    require('/src/assets/project/zhihu/zhihu15.png'),
+    require('/src/assets/project/zhihu/zhihu16.png'),
+    require('/src/assets/project/zhihu/zhihu17.png'),
+    require('/src/assets/project/zhihu/zhihu18.png'),
+    require('/src/assets/project/zhihu/zhihu19.png'),
+    require('/src/assets/project/zhihu/zhihu20.png'),
+    require('/src/assets/project/zhihu/zhihu21.png'),
+    require('/src/assets/project/zhihu/zhihu22.png'),
+    require('/src/assets/project/zhihu/zhihu23.png'),
+    require('/src/assets/project/zhihu/zhihu24.png'),
+    require('/src/assets/project/zhihu/zhihu25.png'),
+    require('/src/assets/project/zhihu/zhihu26.png'),
+    require('/src/assets/project/zhihu/zhihu27.png'),
+    require('/src/assets/project/zhihu/zhihu28.png')
+    ]
+
+const scard4 ='/home/project/ZHIHU'
+
+
+export { scard1 } //名称
+export { scard2 } //卡片背景
+export { scard3 } //内容
+export { scard4 } //页面地址
